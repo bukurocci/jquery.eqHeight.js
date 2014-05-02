cfc574bf80daebf1716f940d7c181942a7461a11
Naokazu Shimabukuro <bukurocci@nsmbp.local>
2014-05-03 07:18:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# jquery.eqheight.js
+
+A jQuery plugin that equalize the height of elements
+
+## Usage
+Specify the parent element of the elements which you would like to equalize the height,
+and call the method for equalizing their height
+
+```js
+$(‘.example’).eqHeight();
+```
+
+## Option
+You can also apply height equalization to each groups.
+if `group` is specified, they are split into groups of N（N is a specified number）
+
+```js
+$(‘.example’).eqHeight({
+	group: 2 
+});
+```
+
+### Methods
+```js
+$(‘.example’).eqHeight(‘refresh’, options);
+```
+Invoke `refresh` method to restore their height and re-equalize them.
+refresh method can takes an options argument.
+
+
+```js
+$(‘.example’).eqHeight(‘destroy’);
+```
+Invoke `destroy` method to restore equalized height to default.
+
+## License
+MIT License
---
+++ jquery.eqheight.js
@@ -0,0 +1,87 @@
+
+
+;(function($, window, undefined) {
+
+	var _toString = {}.toString;
+	var _slice = [].slice;
+
+	var methods = {};
+
+	function equalizeHeight($items) {
+		var heights = [];
+
+		$items.each(function() {
+
+			heights.push($(this).height());
+		});
+
+		$items.height(Math.max.apply(null, heights));
+	}
+
+	methods.init = function(options) {
+		
+		options = $.extend(true, {}, $.fn.eqHeight.defaultOptions, options);
+
+		return this.each(function() {
+			var $el = $(this);
+			var $children = $el.children(':visible');
+			var group = options.group;
+			var numRows;
+
+			console.log($children)
+
+			if(group === 0) {
+
+				equalizeHeight($children);
+			} else {
+
+				numRows = Math.ceil($children.length / group);
+
+				for(var i=0, iz=numRows; i<iz; i++) {
+
+					equalizeHeight($children.slice(group*i, group*i+group));
+				}
+			}
+
+			$el.data('eqheightOption', options);
+		});
+	};
+
+	methods.refresh = function(options) {
+
+		if(!options) {
+			options = this.data('eqheightOption');
+		}
+
+		this.children(':visible').css('height', 'auto').end().eqHeight(options);
+	};
+
+	methods.destroy = function() {
+
+		this.children(':visible').css('height', 'auto').end().removeData('eqheightOption');
+	};
+
+	$.fn.eqHeight = function() {
+
+		var first = arguments[0] ;
+		var rest = _slice.call(arguments, 1);
+
+		if(!arguments.length || $.isPlainObject(first)) {
+			methods.init.call(this, first);
+		}
+
+		if(_toString.call(arguments[0]) === '[object String]') {
+			
+			if(first in methods) {
+
+				methods[first].apply(this, rest);
+			}
+		}
+
+		return this;
+	}
+
+	$.fn.eqHeight.defaultOptions = {
+		group: 0
+	};
+})(jQuery, window);
